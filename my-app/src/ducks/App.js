815dc650c99a12cb4a1e6f5380e196821ea4b87e
Tarun Dugar <tarundugar1992@gmail.com>
2017-05-28 22:24:28 +0530

    create-duck, creat-actiontypes start

---
+++ my-app/src/ducks/App.js
@@ -0,0 +1,29 @@
+import { fromJS } from 'immutable';
+
+const initialState = fromJS({
+  'isLoading': false
+});
+
+// action types
+const LOADING = 'LOADING';
+
+// reducer
+export default function reducer(state=initialState, action={}) {
+  switch (action.type) {
+    case LOADING: return state.merge(fromJS({
+      'isLoading': true
+    }));
+    default: return state;
+  }
+}
+
+
+// action creators
+const appActions = {
+  loadData() {
+    return {
+      'type': LOADING
+    };
+  } 
+};
+export appActions;
